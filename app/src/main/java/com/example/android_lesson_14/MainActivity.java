1df6c6fb81e333477a11113ecf79a664d38607c5
Ben2020 <ben.atoyan@mail.ru>
2020-11-10 15:33:36 +0400

    commit

--- app/src/main/java/com/example/android_lesson_14/MainActivity.java
+++ app/src/main/java/com/example/android_lesson_14/MainActivity.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("my", "hello");
+        Toast.makeText(this, "my", Toast.LENGTH_SHORT).show();
     }
 }
