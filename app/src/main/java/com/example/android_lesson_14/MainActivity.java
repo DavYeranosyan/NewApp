465ca51bd427e281901e5d25ddf59bdc45bbb445
Ben2020 <ben.atoyan@mail.ru>
2020-11-10 15:30:43 +0400

    commit

--- app/src/main/java/com/example/android_lesson_14/MainActivity.java
+++ app/src/main/java/com/example/android_lesson_14/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_lesson_14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("my", "hello");
+
     }
 }
