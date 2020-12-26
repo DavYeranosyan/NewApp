874c8bce1f8122eb5fe6af4c904e9ecdd938aab6
Dav <eranosyan.davit1@mail.ru>
2020-12-26 18:00:26 +0400

    commit

--- app/src/main/java/com/example/android_lesson_14/authentication/LoginActivity.java
+++ app/src/main/java/com/example/android_lesson_14/authentication/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.android_lesson_14.authentication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
     EditText email, password;
@@ -37,6 +43,33 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
                     }else {
                         if(firebaseAuth.getCurrentUser().isEmailVerified()){
+                            String noteId = "com.example.android_lesson_14.authentication";
+                            NotificationCompat.Builder builder =
+                                    new NotificationCompat.Builder(LoginActivity.this, noteId)
+                                            .setAutoCancel(true)
+                                            .setDefaults(Notification.DEFAULT_ALL)
+                                            .setSmallIcon(R.drawable.ic_baseline_delete_outline_24)
+                                            .setContentInfo("info")
+                                            .setWhen(System.currentTimeMillis())
+                                            .setContentTitle("Title")
+                                            .setContentText("You login in account  "+firebaseAuth.getCurrentUser().getDisplayName())
+                                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                            NotificationManager notificationManager =
+                                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                            {
+                                String channelId = "Your_channel_id";
+                                NotificationChannel channel = new NotificationChannel(
+                                        channelId,
+                                        "Channel human readable title",
+                                        NotificationManager.IMPORTANCE_HIGH);
+                                notificationManager.createNotificationChannel(channel);
+                                builder.setChannelId(channelId);
+                            }
+                            notificationManager.notify(1, builder.build());
+
+
                             startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
                         }else{
                             Toast.makeText(getApplicationContext(), "Email is invalid verified", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/android_lesson_14/authentication/SendMail.java
+++ app/src/main/java/com/example/android_lesson_14/authentication/SendMail.java
@@ -1,6 +1,5 @@
 package com.example.android_lesson_14.authentication;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
